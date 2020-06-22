0090d9741a731769012a21cf499b6d1c9919314a
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-06-22 20:16:27 +0000

    assertEqual first element in array

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`;
+  }
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
