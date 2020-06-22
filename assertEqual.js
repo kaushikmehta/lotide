5ce75acd2feba682dad10ef3dc29d1ac21b3851d
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-06-22 20:05:21 +0000

    added emojis to assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return "✅✅✅ Assertion Passed: " + actual + " === " + expected;
+  } else {
+    return "🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected;
+  }
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual('hello', 'hello'));
+console.log(assertEqual('hello', 'Hello'));
+console.log(assertEqual(27, 27));
+console.log(assertEqual(27, '27'));
