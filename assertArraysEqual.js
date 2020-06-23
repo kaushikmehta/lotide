6275699b1d3a989a19fe29c4ea7ea37ba522fb3d
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-06-23 19:52:18 +0000

    Logging messages to console as opposed to returning values

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+const eqArrays = function(array1, array2){
+  if (array1.length === array2.length) {
+    let value = true
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]){
+        value = false
+      }
+    }
+    value === false ? console.log("🛑 The two arrays are same in size but not equal") : console.log("✅ The two arrays are equal");
+  } else {
+    console.log("The two arrays are different sizes and therefore not equal");
+  }
+};
+
+
+eqArrays([1, 2, 3], [1, 2, 3]);
+eqArrays([1, 2, 3], [3, 2, 1]);
