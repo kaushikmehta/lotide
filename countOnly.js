0cb1d78fe9a2dfcb1002ca64b34733ce8e9f695b
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-06-24 18:02:20 +0000

    function that counts how many times a variable occurs in a list if we need to count it

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`;
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let countedNames = {};
+  for (let item in itemsToCount) {
+    // console.log(itemsToCount[item]);
+   if (itemsToCount[item] === true) {
+    for (let name of allItems) {
+      if (name === item){
+        if (countedNames[name] === undefined) {
+          countedNames[name] = 1
+        } else {
+          countedNames[name] +=1
+        } // else
+
+      } // if name = item
+    }
+   }
+  }
+  // console.log("countedObj: ", countedNames)
+  return countedNames;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+console.log(assertEqual(result1["Jason"], 1));
+console.log(assertEqual(result1["Karima"], undefined));
+console.log(assertEqual(result1["Fang"], 2));
