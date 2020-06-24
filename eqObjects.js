05049c38be3131641337743dec59877e943135f1
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-06-24 20:52:37 +0000

    empty function and test cases

---
+++ eqObjects.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`;
+  }
+};
+
+const eqObjects = function (object1, object2) {
+
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(assertEqual(eqObjects(ab, ba), true)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(assertEqual(eqObjects(ab, abc), true)); // => false
