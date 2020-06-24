09e139b2e900c6414b54c15d2b33eb9fbaf717e7
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-06-24 22:08:33 +0000

    implemented primitives as values

--- eqObjects.js
+++ eqObjects.js
@@ -7,8 +7,15 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqObjects = function (object1, object2) {
-
+  let result = false;
+  for (key1 in object1){
+    if (Object.keys(object2).indexOf(key1) !== -1 ){
+      result = true;
+    }
+  }
+  return result;
 };
+
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
@@ -16,3 +23,7 @@ console.log(assertEqual(eqObjects(ab, ba), true)); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
 console.log(assertEqual(eqObjects(ab, abc), true)); // => false
+
+
+// console.log(eqObjects(ab, abc));
+// console.log(eqObjects(ab, ba));
