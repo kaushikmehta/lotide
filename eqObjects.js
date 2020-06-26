8c36973562ba5e2e3b1e95c65061d75aeae4a7c5
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-06-26 23:37:06 +0000

    added recursive function to validate objects within objects

--- eqObjects.js
+++ eqObjects.js
@@ -29,29 +29,36 @@ const eqObjects = function (object1, object2) {
   for (key1 of keys1) {
     const firstVal = object1[key1];
     const secVal = object2[key1];
+
     if (Array.isArray(object1[key1]) && Array.isArray(object2[key1])) {
       if(!eqArrays(object1[key1], object2[key1])){
         return false
       };
     } else {
-      if (object1[key1] !== object2[key1]){
+      if (typeof firstVal === "object" && typeof secVal === "object" ){
+          return eqObjects(firstVal, secVal);
+      } else if (object1[key1] !== object2[key1]){
         return false
       }
-    }
+    } // else 
   };
   return true;;
 };
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertEqual(eqObjects(ab, ba), true); // => true
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+// const abc = { a: "1", b: "2", c: "3" };
+// assertEqual(eqObjects(ab, abc), false); // => false
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true); // => true
+
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false); // => false
