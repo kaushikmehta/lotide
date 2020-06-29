f7afa1b428db0bc18474aed700bcd0c5a0acf952
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-06-29 17:48:02 +0000

    added mocha and chai libraries and incorporated them in headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,20 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const head = require("../head");
 
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+describe("#head", () => {
+  it("should retun 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("should retun 5 for [5]", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+  it("should retun 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("should retun Hello for [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+});
